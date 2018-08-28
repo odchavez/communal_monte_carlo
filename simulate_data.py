95683b002ae14fdd6fee583b3df67f3c52b67a1e
Omar Chavez <omar.chavez@opcity.com>
2018-08-28 12:38:36 -0500

    fixed simulated data simulator and updated CMC_tests to test data generation

--- simulate_data.py
+++ simulate_data.py
@@ -32,15 +32,22 @@ class simulated_data:
             self.N=params['N']
             self.N_batch = params['N_batch']
             self.shards=params['shards']
-            self.X={}
-            self.Y={}
+            #self.X={}
+            #self.Y={}
             f=1.0
             x = np.arange(self.N) # the points on the x axis for plotting
             self.b=np.zeros((self.N,self.p))
             
+            output = {}
             for m in range(self.shards):
-                self.X["shard_"+str(m)]={}
-                self.Y["shard_"+str(m)]={}
+                output["shard_"+str(m)]     ={}
+                output["shard_"+str(m)]['X']={}
+                output["shard_"+str(m)]['Y']={}
+            
+            print('output=',output)
+            #for m in range(self.shards):
+            #    self.X["shard_"+str(m)]={}
+            #    self.Y["shard_"+str(m)]={}
             
             for os in range(self.p):
                 self.b[:,os] = [ 1.0*np.sin(np.pi*f * (i/(self.N*1.0)) + omega_shift[os]) for i in x]
@@ -54,18 +61,22 @@ class simulated_data:
             
             data_index=0
             for i in range(self.N):
-                key=str(i)+":"+str(i%self.shards)+":"+str(data_index)
-                print('key=',key)
+                #key=str(i)+":"+str(i%self.shards)+":"+str(data_index)
+                key=str(i)+":"+str(data_index)
+                
+                #print('key=',key)
                 s="shard_"+str(i%self.shards)
-                self.X[s][key] = np.random.uniform(-1,1,self.p*self.N_batch).reshape((self.N_batch,self.p))
-                self.Y[s][key] = np.zeros(self.N_batch)
-                inner=self.X[s][key].dot(self.b[i,])
+                #print('s=',s)
+                #print('output[s]=', output[s])
+                output[s]['X'][key] = np.random.uniform(-1,1,self.p*self.N_batch).reshape((self.N_batch,self.p))
+                output[s]['Y'][key] = np.zeros(self.N_batch)
+                inner=output[s]['X'][key].dot(self.b[i,])
                 #print('innter=',inner)
                 samp=np.random.normal(loc=inner, scale=1.0)
                 #print("samp=",samp)
                 for j in range(self.N_batch):
                     if samp[j]>=0:
-                        self.Y[s][key][j]=1
+                        output[s]['Y'][key][j]=1
                 
                 if i%self.shards == self.shards-1:
                     data_index+=1
@@ -73,7 +84,14 @@ class simulated_data:
             sig=np.max(np.var(self.b[0:(self.N-1),:]-self.b[1:,:], axis=0))
             print("sig=", sig)
             self.B=sig*params['B']*2
-            self.output={'X':self.X, 'Y':self.Y, 'N': self.N, 'b': self.b, 'B':self.B, 'model':self.model}
+            
+            for m in range(self.shards):
+                output["shard_"+str(m)]['N'] = self.N
+                output["shard_"+str(m)]['b'] = self.b
+                output["shard_"+str(m)]['B'] = self.B
+                output["shard_"+str(m)]['model'] = self.model
+                
+            self.output=output#{'X':self.X, 'Y':self.Y, 'N': self.N, 'b': self.b, 'B':self.B, 'model':self.model}
             
     def get_data(self):
         return(self.output)
