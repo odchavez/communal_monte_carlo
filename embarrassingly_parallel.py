6f2b0e0e5efce031d9b31900a32e07b40983ce38
Omar Chavez <omar.chavez@opcity.com>
2018-09-18 11:35:38 -0500

    added but did not complete plotting of CMC particle filter results

--- embarrassingly_parallel.py
+++ embarrassingly_parallel.py
@@ -21,10 +21,13 @@ class embarrassingly_parallel:
     
     def run_batch(self, data):
         for m in range(self.number_of_shards):
-            self.pf_obj[m].update_data(self.data['shard_'+str(m)])
+            self.pf_obj[m].update_data(data['shard_'+str(m)])
             self.pf_obj[m].run_particle_filter()
     
-    
+    #def update_parallel_pf_data(self, data):
+    #    for m in range(self.number_of_shards):
+    #        self.pf_obj[m].update_data(self.data['shard_'+str(m)])
+            
     def plot_parameter_path(self, particle_prop=0.01):
         print("plot_parameter_path...")
         
@@ -35,6 +38,7 @@ class embarrassingly_parallel:
         
         for os in range(param_num):
           temp_all_parts = np.zeros((len(particle_indices), total_time_steps))
+          print("data keys = ", self.data.keys())
           for sn in range(self.data['parallel_shards']):
             for pn in range(len(particle_indices)):
               particle=self.pf_obj[sn].get_particle(particle_indices[pn])
@@ -46,8 +50,6 @@ class embarrassingly_parallel:
           #  ts_values=self.params['epoch_at'][ts]
           #  temp_all_parts[:,ts_values]=temp_all_parts[:,ts_values]/self.data['parallel_shards']      
           params.append(temp_all_parts)
-        
-        
         
         for par_n in range(param_num):
             avg_param_0=np.mean(params[par_n], axis=0)
@@ -70,6 +72,51 @@ class embarrassingly_parallel:
             plt.yticks(np.linspace(start=min_tic, stop=max_tic, num=12))
             plt.grid(True)
 
+    def plot_CMC_parameter_path(self, particle_prop=0.01):
+        print("plot_parameter_path...")
+        
+        param_num=self.pf_obj[0].get_particle(0).bo_list.shape[1]
+        total_time_steps = len(self.pf_obj[0].get_particle(0).bo_list[:,0])
+        params=list()
+        particle_indices = np.random.choice(self.PART_NUM, max(int(self.PART_NUM*particle_prop), 1))
+        
+        for os in range(param_num):
+          temp_all_parts = np.zeros((len(particle_indices), total_time_steps))
+          print("data keys = ", self.data.keys())
+          for sn in range(self.data['parallel_shards']):
+            for pn in range(len(particle_indices)):
+              particle=self.pf_obj[sn].get_particle(particle_indices[pn])
+              p_temp = particle.bo_list[:,os].copy()
+              p_temp[np.isnan(p_temp)]=0
+              temp_all_parts[pn,:]=np.add(temp_all_parts[pn,:],p_temp)
+            
+            #print("temp_all_parts=",temp_all_parts)
+          #for ts in range(len(self.params['epoch_at'])):
+          #  ts_values=self.params['epoch_at'][ts]
+          #  temp_all_parts[:,ts_values]=temp_all_parts[:,ts_values]/self.data['parallel_shards']      
+          params.append(temp_all_parts)
+        
+        for par_n in range(param_num):
+            avg_param_0=np.mean(params[par_n], axis=0)
+            std_parma_0=np.std(params[par_n], axis=0)
+            above=np.add(avg_param_0,std_parma_0*2)
+            below=np.add(avg_param_0,-std_parma_0*2)
+            
+            truth=self.data['b'][:,par_n]#data['shard_0']['b'][:,par_n]
+            
+            x = np.arange(len(avg_param_0)) # the points on the x axis for plotting
+            
+            fig, ax1 = plt.subplots()
+            plt.plot(x,truth,'black')
+            ax1.fill_between(x, below, above, facecolor='green',  alpha=0.3)
+            plt.plot(x,avg_param_0, 'b', alpha=.8)
+            for line_tick in self.params['epoch_at']:
+                plt.axvline(x=line_tick, color='r', alpha=0.25)
+            min_tic=np.min([np.min(below),np.min(truth)])
+            max_tic=np.max([np.max(above),np.max(truth)])
+            plt.yticks(np.linspace(start=min_tic, stop=max_tic, num=12))
+            plt.grid(True)
+            
     def shuffel_embarrassingly_parallel_particles(self, machine_list=None, method='uniform'):
         
         self.all_particles=list()
