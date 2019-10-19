ab3720b77a689a84f2cc7b675ad667e756731f9e
Omar Chavez <omar.chavez@opcity.com>
2019-10-19 16:32:14 -0500

    making a 20 pred dataset

--- run_simulate_data.py
+++ run_simulate_data.py
@@ -15,12 +15,17 @@ def get_args():
         help='The number of observations to have per epoch, ie. before a communication step.',
         required=True
     )
+    parser.add_argument(
+        '--predictor_N', type=int,
+        help='The number of observations to have per epoch, ie. before a communication step.',
+        required=True
+    )
     return parser.parse_args()
 
 if __name__ == "__main__":
     
     args = get_args()
     
-    sim_data_obj = sd.simulated_data2(n_per_file = args.Epoch_N, N_total = args.N_total)
+    sim_data_obj = sd.simulated_data2(n_per_file = args.Epoch_N, N_total = args.N_total, pred_number=args.predictor_N)
     sim_data_obj.generate_Betas()
     sim_data_obj.generate_data()
