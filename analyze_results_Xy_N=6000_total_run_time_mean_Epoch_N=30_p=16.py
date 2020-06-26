923666a3bcecf81ebb565ced600d0b2e21c864c5
Omar Chavez <omar.chavez@opcity.com>
2020-06-26 14:54:57 -0500

    fix typo

--- analyze_results_Xy_N=6000_total_run_time_mean_Epoch_N=30_p=16.py
+++ analyze_results_Xy_N=6000_total_run_time_mean_Epoch_N=30_p=16.py
@@ -225,7 +225,7 @@ predictors = ['B_0','B_1','B_2','B_3','B_4','B_5','B_6','B_7','B_8','B_9','B_10'
 ############################
 ##          p=16
 ############################
-#part_num = [1000]
+part_num = [1000]
 #
 #big_results_dict = af.prep_big_results_dict(
 #    f_shard_number = [3],
