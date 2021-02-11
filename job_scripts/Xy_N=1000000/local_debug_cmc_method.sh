64dad70cac87bc186caace75ecb1b631a3257e90
omar chavez <omarchavez@omars-MacBook-Pro.local>
2021-02-10 18:53:07 -0600

    broke up job scripts for 1000000 obs jobs

--- job_scripts/Xy_N=1000000/local_debug_cmc_method.sh
+++ job_scripts/Xy_N=1000000/local_debug_cmc_method.sh
@@ -1,42 +1,11 @@
 #!/bin/bash
 
-mpirun -np 3 python mpi_emb_par_sim_dat_with_comm.py  --Xy_N 6000 --Epoch_N 6000 --Nt 30 --p 2 --N_Node 3 --particles_per_shard 20 --experiment_number 999 --save_history 0 --GP_version 0 --randomize_shards 0 --files_to_process_path synth_data/Xy_N=6000_Epoch_N=1200_Nt=30_p=2/GP_version=0 --results_sub_folder synth_data --source_folder synth_data
+mpirun -np 4 python mpi_emb_par_sim_dat_with_comm.py  --Xy_N 1000000 --Epoch_N 100000 --Nt 1000 --p 100 --N_Node 4 --particles_per_shard 2 --experiment_number 0 --save_history 0 --GP_version 0 --randomize_shards 0 --files_to_process_path synth_data/Xy_N=1000000_Epoch_N=10000_Nt=1000_p=100/GP_version=0 --results_sub_folder synth_data --source_folder synth_data
 
-mpirun -np 3 python mpi_emb_par_sim_dat_with_comm.py  --Xy_N 6000 --Epoch_N 6000 --Nt 30 --p 2 --N_Node 3 --particles_per_shard 20 --experiment_number 999 --save_history 0 --GP_version 1 --randomize_shards 0 --files_to_process_path synth_data/Xy_N=6000_Epoch_N=1200_Nt=30_p=2/GP_version=1 --results_sub_folder synth_data --source_folder synth_data
+mpirun -np 4 python mpi_emb_par_sim_dat_with_comm.py  --Xy_N 1000000 --Epoch_N 100000 --Nt 1000 --p 100 --N_Node 4 --particles_per_shard 10 --experiment_number 1 --save_history 0 --GP_version 0 --randomize_shards 0 --files_to_process_path synth_data/Xy_N=1000000_Epoch_N=10000_Nt=1000_p=100/GP_version=0 --results_sub_folder synth_data --source_folder synth_data --global_weighting kernel_weighting
 
-mpirun -np 3 python mpi_emb_par_sim_dat_with_comm.py  --Xy_N 6000 --Epoch_N 6000 --Nt 30 --p 2 --N_Node 3 --particles_per_shard 20 --experiment_number 999 --save_history 0 --GP_version 2 --randomize_shards 0 --files_to_process_path synth_data/Xy_N=6000_Epoch_N=1200_Nt=30_p=2/GP_version=2 --results_sub_folder synth_data --source_folder synth_data
-
-mpirun -np 3 python mpi_emb_par_sim_dat_with_comm.py  --Xy_N 6000 --Epoch_N 6000 --Nt 30 --p 2 --N_Node 3 --particles_per_shard 20 --experiment_number 999 --save_history 0 --GP_version 3 --randomize_shards 0 --files_to_process_path synth_data/Xy_N=6000_Epoch_N=1200_Nt=30_p=2/GP_version=3 --results_sub_folder synth_data --source_folder synth_data
-
-mpirun -np 3 python mpi_emb_par_sim_dat_with_comm.py  --Xy_N 6000 --Epoch_N 6000 --Nt 30 --p 2 --N_Node 3 --particles_per_shard 20 --experiment_number 999 --save_history 0 --GP_version 4 --randomize_shards 0 --files_to_process_path synth_data/Xy_N=6000_Epoch_N=1200_Nt=30_p=2/GP_version=4 --results_sub_folder synth_data --source_folder synth_data
-
-mpirun -np 3 python mpi_emb_par_sim_dat_with_comm.py  --Xy_N 6000 --Epoch_N 6000 --Nt 30 --p 2 --N_Node 3 --particles_per_shard 20 --experiment_number 999 --save_history 0 --GP_version 5 --randomize_shards 0 --files_to_process_path synth_data/Xy_N=6000_Epoch_N=1200_Nt=30_p=2/GP_version=5 --results_sub_folder synth_data --source_folder synth_data
-
-mpirun -np 3 python mpi_emb_par_sim_dat_with_comm.py  --Xy_N 6000 --Epoch_N 6000 --Nt 30 --p 2 --N_Node 3 --particles_per_shard 20 --experiment_number 999 --save_history 0 --GP_version 6 --randomize_shards 0 --files_to_process_path synth_data/Xy_N=6000_Epoch_N=1200_Nt=30_p=2/GP_version=6 --results_sub_folder synth_data --source_folder synth_data
-
-mpirun -np 3 python mpi_emb_par_sim_dat_with_comm.py  --Xy_N 6000 --Epoch_N 6000 --Nt 30 --p 2 --N_Node 3 --particles_per_shard 20 --experiment_number 999 --save_history 0 --GP_version 7 --randomize_shards 0 --files_to_process_path synth_data/Xy_N=6000_Epoch_N=1200_Nt=30_p=2/GP_version=7 --results_sub_folder synth_data --source_folder synth_data
-
-mpirun -np 3 python mpi_emb_par_sim_dat_with_comm.py  --Xy_N 6000 --Epoch_N 6000 --Nt 30 --p 2 --N_Node 3 --particles_per_shard 20 --experiment_number 999 --save_history 0 --GP_version 8 --randomize_shards 0 --files_to_process_path synth_data/Xy_N=6000_Epoch_N=1200_Nt=30_p=2/GP_version=8 --results_sub_folder synth_data --source_folder synth_data
-
-mpirun -np 3 python mpi_emb_par_sim_dat_with_comm.py  --Xy_N 6000 --Epoch_N 6000 --Nt 30 --p 2 --N_Node 3 --particles_per_shard 20 --experiment_number 999 --save_history 0 --GP_version 9 --randomize_shards 0 --files_to_process_path synth_data/Xy_N=6000_Epoch_N=1200_Nt=30_p=2/GP_version=9 --results_sub_folder synth_data --source_folder synth_data
+mpirun -np 4 python MPI_LFCMC.py  --Xy_N 1000000 --Epoch_N 100000 --Nt 1000 --p 100 --N_Node 4 --particles_per_shard 10 --experiment_number 2 --save_history 0 --GP_version 0 --randomize_shards 0 --files_to_process_path synth_data/Xy_N=1000000_Epoch_N=10000_Nt=1000_p=100/GP_version=0 --results_sub_folder synth_data --source_folder synth_data
 
 
-mpirun -np 3 python mpi_emb_par_sim_dat_with_comm.py  --Xy_N 6000 --Epoch_N 300 --Nt 30 --p 2 --N_Node 3 --particles_per_shard 20 --experiment_number 999 --save_history 0 --GP_version 0 --randomize_shards 0 --files_to_process_path synth_data/Xy_N=6000_Epoch_N=1200_Nt=30_p=2/GP_version=0 --results_sub_folder synth_data --source_folder synth_data
+mpirun -np 4 python mpi_emb_par_sim_dat_with_comm.py  --Xy_N 1000000 --Epoch_N 100000 --Nt 1000 --p 100 --N_Node 4 --particles_per_shard 10 --experiment_number 3 --save_history 0 --GP_version 0 --randomize_shards 0 --files_to_process_path synth_data/Xy_N=1000000_Epoch_N=10000_Nt=1000_p=100/GP_version=0 --results_sub_folder synth_data --source_folder synth_data --global_weighting normal_consensus_weighting
 
-mpirun -np 3 python mpi_emb_par_sim_dat_with_comm.py  --Xy_N 6000 --Epoch_N 300 --Nt 30 --p 2 --N_Node 3 --particles_per_shard 20 --experiment_number 999 --save_history 0 --GP_version 1 --randomize_shards 0 --files_to_process_path synth_data/Xy_N=6000_Epoch_N=1200_Nt=30_p=2/GP_version=1 --results_sub_folder synth_data --source_folder synth_data
-
-mpirun -np 3 python mpi_emb_par_sim_dat_with_comm.py  --Xy_N 6000 --Epoch_N 300 --Nt 30 --p 2 --N_Node 3 --particles_per_shard 20 --experiment_number 999 --save_history 0 --GP_version 2 --randomize_shards 0 --files_to_process_path synth_data/Xy_N=6000_Epoch_N=1200_Nt=30_p=2/GP_version=2 --results_sub_folder synth_data --source_folder synth_data
-
-mpirun -np 3 python mpi_emb_par_sim_dat_with_comm.py  --Xy_N 6000 --Epoch_N 300 --Nt 30 --p 2 --N_Node 3 --particles_per_shard 20 --experiment_number 999 --save_history 0 --GP_version 3 --randomize_shards 0 --files_to_process_path synth_data/Xy_N=6000_Epoch_N=1200_Nt=30_p=2/GP_version=3 --results_sub_folder synth_data --source_folder synth_data
-
-mpirun -np 3 python mpi_emb_par_sim_dat_with_comm.py  --Xy_N 6000 --Epoch_N 300 --Nt 30 --p 2 --N_Node 3 --particles_per_shard 20 --experiment_number 999 --save_history 0 --GP_version 4 --randomize_shards 0 --files_to_process_path synth_data/Xy_N=6000_Epoch_N=1200_Nt=30_p=2/GP_version=4 --results_sub_folder synth_data --source_folder synth_data
-
-mpirun -np 3 python mpi_emb_par_sim_dat_with_comm.py  --Xy_N 6000 --Epoch_N 300 --Nt 30 --p 2 --N_Node 3 --particles_per_shard 20 --experiment_number 999 --save_history 0 --GP_version 5 --randomize_shards 0 --files_to_process_path synth_data/Xy_N=6000_Epoch_N=1200_Nt=30_p=2/GP_version=5 --results_sub_folder synth_data --source_folder synth_data
-
-mpirun -np 3 python mpi_emb_par_sim_dat_with_comm.py  --Xy_N 6000 --Epoch_N 300 --Nt 30 --p 2 --N_Node 3 --particles_per_shard 20 --experiment_number 999 --save_history 0 --GP_version 6 --randomize_shards 0 --files_to_process_path synth_data/Xy_N=6000_Epoch_N=1200_Nt=30_p=2/GP_version=6 --results_sub_folder synth_data --source_folder synth_data
-
-mpirun -np 3 python mpi_emb_par_sim_dat_with_comm.py  --Xy_N 6000 --Epoch_N 300 --Nt 30 --p 2 --N_Node 3 --particles_per_shard 20 --experiment_number 999 --save_history 0 --GP_version 7 --randomize_shards 0 --files_to_process_path synth_data/Xy_N=6000_Epoch_N=1200_Nt=30_p=2/GP_version=7 --results_sub_folder synth_data --source_folder synth_data
-
-mpirun -np 3 python mpi_emb_par_sim_dat_with_comm.py  --Xy_N 6000 --Epoch_N 300 --Nt 30 --p 2 --N_Node 3 --particles_per_shard 20 --experiment_number 999 --save_history 0 --GP_version 8 --randomize_shards 0 --files_to_process_path synth_data/Xy_N=6000_Epoch_N=1200_Nt=30_p=2/GP_version=8 --results_sub_folder synth_data --source_folder synth_data
-
-mpirun -np 3 python mpi_emb_par_sim_dat_with_comm.py  --Xy_N 6000 --Epoch_N 300 --Nt 30 --p 2 --N_Node 3 --particles_per_shard 20 --experiment_number 999 --save_history 0 --GP_version 9 --randomize_shards 0 --files_to_process_path synth_data/Xy_N=6000_Epoch_N=1200_Nt=30_p=2/GP_version=9 --results_sub_folder synth_data --source_folder synth_data
