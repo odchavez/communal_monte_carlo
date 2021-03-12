3352423ae79115849426a1eb505833820ab8ee75
omar chavez <omarchavez@omars-MacBook-Pro.local>
2021-03-12 09:19:35 -0600

    check point with running fast pf

--- run_simulate_data.py
+++ run_simulate_data.py
@@ -1,6 +1,9 @@
 """
-run with: python run_simulate_data.py --N_total 400000 --Epoch_N 2000 --predictor_N 32 --N_per_tic 100 --GP 1 --GP_version 0
+classification run with: python run_simulate_data.py --N_total 400000 --Epoch_N 2000 --predictor_N 32 --N_per_tic 100 --GP 1 --GP_version 0 
+
+regression run with: python run_simulate_data.py --N_total 100000 --Epoch_N 10000 --predictor_N 32 --N_per_tic 1 --GP_version 0 --model_type regression --regression_error 1.0
 """
+
 import argparse
 import simulate_data as sd
 
@@ -63,25 +66,49 @@ def get_args():
         help='use Gaussian Process generated Betas - defauts to 0.  Select GP=1 to generate GP Betas',
         required=False, default= 0
     )
+    parser.add_argument(
+        '--model_type', type=str,
+        help='create classification or regression data',
+        required=False, default= "classification"
+    )
+    parser.add_argument(
+        '--regression_error', type=float,
+        help='Amount of noise to add to Y = X*Beta + error',
+        required=False, default= 1.
+    )
     return parser.parse_args()
 
 if __name__ == "__main__":
     
     args = get_args()
-    
-    sim_data_obj = sd.simulated_data2(
-        n_per_file = args.Epoch_N, 
-        N_total = args.N_total, 
-        n_per_tic = args.N_per_tic, 
-        pred_number=args.predictor_N,
-        seed = args.data_seed,
-        GP=args.GP,
-        GB_version=args.GP_version
-    )
-    sim_data_obj.generate_Betas(
-        sin_or_cos = args.sin_or_cos, 
-        intercepts_1=args.line_1, 
-        intercepts_2=args.line_2, 
-        intercepts_3=args.line_3,   
-    )
-    sim_data_obj.generate_data()
+    if args.model_type == "classification":
+        sim_data_obj = sd.simulated_data2(
+            n_per_file = args.Epoch_N, 
+            N_total = args.N_total, 
+            n_per_tic = args.N_per_tic, 
+            pred_number=args.predictor_N,
+            seed = args.data_seed,
+            GP=args.GP,
+            GB_version=args.GP_version
+        )
+        sim_data_obj.generate_Betas(
+            sin_or_cos = args.sin_or_cos, 
+            intercepts_1=args.line_1, 
+            intercepts_2=args.line_2, 
+            intercepts_3=args.line_3,   
+        )
+        sim_data_obj.generate_data()
+    elif args.model_type == "regression": 
+        sim_data_obj = sd.simulated_data_regression(
+            n_per_file = args.Epoch_N, 
+            N_total = args.N_total, 
+            n_per_tic = args.N_per_tic, 
+            pred_number=args.predictor_N,
+            seed = args.data_seed,
+            GP_version=args.GP_version,
+            err_std = args.regression_error
+        )
+        sim_data_obj.generate_Betas()
+        sim_data_obj.generate_data()
+    else:
+        raise ValueError('{} modle is not implemented'.format(method))
--- simulate_data.py
+++ simulate_data.py
@@ -115,10 +115,14 @@ class simulated_data2:
         print("generating regression coefficients...")
         if self.GP==0:
             self.Beta_vals_base = {
-                'B_0': self.make_sin_wave_trajectory(f_time_tic = self.time_tics, sin_or_cos=sin_or_cos),
-                'B_1': self.make_linear_trajectory(f_time_tic = self.time_tics, y_2 = 2.0, y_1 =  intercepts_1),
-                'B_2': self.make_linear_trajectory(f_time_tic = self.time_tics, y_2 = -3.0, y_1 = intercepts_2),
-                'B_3': self.make_linear_trajectory(f_time_tic = self.time_tics, y_2 = -3.0, y_1 = intercepts_3),
+                'B_0': self.make_sin_wave_trajectory(
+                    f_time_tic = self.time_tics, sin_or_cos=sin_or_cos),
+                'B_1': self.make_linear_trajectory(
+                    f_time_tic = self.time_tics, y_2 = 2.0, y_1 =  intercepts_1),
+                'B_2': self.make_linear_trajectory(
+                    f_time_tic = self.time_tics, y_2 = -3.0, y_1 = intercepts_2),
+                'B_3': self.make_linear_trajectory(
+                    f_time_tic = self.time_tics, y_2 = -3.0, y_1 = intercepts_3),
             }
             beta_cnames = list(range(self.pred_number))
             Beta_vals={}
@@ -128,7 +132,7 @@ class simulated_data2:
             #Beta_vals.head()
             self.Beta_vals_df = pd.DataFrame(Beta_vals)[beta_cnames]
     
-            self.Beta_vals_df.to_csv(self.output_folder_name + "Beta_t.csv" )
+            self.Beta_vals_df.to_csv(self.output_folder_name + "Beta_t.csv" , index=False)
         if self.GP==1:
             beta_cnames = list(range(self.pred_number))
             Beta_vals={}
@@ -137,7 +141,7 @@ class simulated_data2:
                 beta_cnames[pn] = 'B_'+str(pn)
             self.Beta_vals_df = pd.DataFrame(Beta_vals)[beta_cnames]
             
-            self.Beta_vals_df.to_csv(self.output_folder_name + self.Beta_file_name )
+            self.Beta_vals_df.to_csv(self.output_folder_name + self.Beta_file_name , index=False)
 
         print("regression coefficients generation complete...")
         print("estimating Tau_inv_std parameter...")
@@ -181,8 +185,8 @@ class simulated_data2:
             Y_vals = np.random.binomial(n=1, p=event_prob, size=None)
 
             X_i['y'] = Y_vals
-            X_i['Tau_inv_std'] = self.Tau_inv_std
-            X_i['Bo_std'] = self.Bo_std
+            #X_i['Tau_inv_std'] = self.Tau_inv_std
+            #X_i['Bo_std'] = self.Bo_std
             X_i['time'] = tt
 
             if X_i_all.shape[0]==0:
@@ -196,7 +200,7 @@ class simulated_data2:
                     ".csv"
                 )
 
-                X_i_all.to_csv(self.output_folder_name + file_name )
+                X_i_all.to_csv(self.output_folder_name + file_name , index=False, header=False)
 
                 file_num+=1
                 X_i_all = pd.DataFrame()
@@ -326,4 +330,144 @@ class simulated_data_Dynamic_Gaussian_Mixture:
                     temp_df['scale'] = all_component_scale
                     output = output.append(temp_df)
         self.data = output.reset_index(drop=True)
-        self.data.to_csv(self.output_folder_name + "fn=0.csv" )
+        self.data.to_csv(self.output_folder_name + "fn=0.csv" , index=False, header=False)
+
+        
+class simulated_data_regression:
+
+    def __init__(self, n_per_file, N_total = 10000000, n_per_tic = 1, pred_number = 32, seed = 0, GP_version=0, err_std=1):
+
+        self.time_tics = np.array(range(int(N_total/n_per_tic)))
+        self.row = len(self.time_tics) * n_per_tic
+        self.n_per_file = n_per_file/n_per_tic
+        self.pred_number =pred_number
+        self.N_total = N_total
+        self.n_per_tic = n_per_tic
+        self.n_per_epoch = n_per_file
+        self.seed = seed
+        self.GP_version = GP_version
+        self.error_std=err_std
+        
+        self.output_folder_name = (
+            "synth_data/regression/"
+            "Xy_N=" + str(self.N_total) +
+            "_Epoch_N=" + str(self.n_per_epoch) +
+            "_Nt=" + str(self.n_per_tic) +
+            "_p=" + str(self.pred_number) +
+            "/GP_version=" + str(self.GP_version) +
+            "/"
+        )
+        
+        self.Beta_file_name = (
+            "Beta_t" + 
+            "_Xy_N=" + str(self.N_total) +
+            "_Epoch_N=" + str(self.n_per_epoch) +
+            "_Nt=" + str(self.n_per_tic) +
+            "_p=" + str(self.pred_number) +
+            "_GP_version=" + str(self.GP_version) +
+            ".csv"
+        )
+                
+        if not os.path.exists(self.output_folder_name):
+            os.makedirs(self.output_folder_name)
+
+    def make_K(self, x, h, lam):
+        """
+        Make covariance matrix from covariance kernel
+        """
+        # for a data array of length x, make a covariance matrix x*x:
+        K = np.zeros((len(x),len(x)))
+        for i in range(0,len(x)):
+            for j in range(0,len(x)):
+                # calculate value of K for each separation:
+                K[i,j] = self.cov_kernel(x[i],x[j],h,lam)
+    
+        return K
+
+    def cov_kernel(self, x1,x2,h,lam):
+        """
+        Squared-Exponential covariance kernel
+        """
+        k12 = h**2*np.exp(-1.*(x1 - x2)**2/lam**2)
+        return k12
+    
+    def make_GP_trajectory(self, f_time_tic):
+        #t = np.arange(0, f_time_tic)
+        h=1.0
+        lam=len(f_time_tic)/2 #100
+        K = self.make_K(f_time_tic,h,lam)
+        y = np.random.multivariate_normal(np.zeros(len(f_time_tic)),K)
+        return y-y[0]
+
+    def generate_Betas(self):
+        print("generating regression coefficients...")
+        beta_cnames = list(range(self.pred_number))
+        Beta_vals={}
+        for pn in tqdm(range(self.pred_number)):
+            Beta_vals['B_'+str(pn)] = self.make_GP_trajectory(f_time_tic= self.time_tics)
+            beta_cnames[pn] = 'B_'+str(pn)
+        self.Beta_vals_df = pd.DataFrame(Beta_vals)[beta_cnames]
+        
+        self.Beta_vals_df.to_csv(self.output_folder_name + self.Beta_file_name , index=False)
+
+        print("regression coefficients generation complete...")
+        print("estimating Tau_inv_std parameter...")
+        self.Tau_inv_std = np.max(self.Beta_vals_df.diff().std())
+        self.Bo_std = self.Beta_vals_df.values.std()
+        print("Tau_inv_std = ", self.Tau_inv_std)
+        print("Bo_std = ", self.Bo_std)
+        
+
+    def generate_data(self):
+
+        print("generating data...")
+        print("writing data to " + self.output_folder_name)
+        X_i_all = pd.DataFrame()
+        vcnames=list(range(self.pred_number))
+        #pnames={}
+        for i in range(self.pred_number):
+            vn="v_"+str(i)
+            #pnames["v_"+str(i)] = vn
+            vcnames[i]=vn
+        file_num = 0
+        for tt in tqdm(range(len(self.time_tics))):
+            np.random.seed(tt+self.seed)
+
+            X_i =  pd.DataFrame(
+                np.random.uniform(
+                    low = -1,
+                    high = 1,
+                    size = (self.n_per_tic, self.pred_number)
+                )
+            )
+            X_i.columns=vcnames
+            Beta_t = self.Beta_vals_df.iloc[tt].T
+            X_B_t = X_i.dot(np.array(Beta_t))
+            error = np.random.normal(
+                loc=0, scale=self.error_std, size=self.n_per_tic)
+            Y_vals = X_B_t + error
+
+            X_i['y'] = Y_vals
+            #X_i['Tau_inv_std'] = self.Tau_inv_std
+            #X_i['Bo_std'] = self.Bo_std
+            X_i['time'] = tt
+
+            if X_i_all.shape[0]==0:
+                X_i_all = X_i
+            else:
+                X_i_all = pd.concat([X_i_all, X_i], axis=0)
+
+            if tt % self.n_per_file == self.n_per_file-1:
+                file_name = (
+                    "fn=" +str(file_num) +
+                    ".csv"
+                )
+
+                X_i_all.to_csv(self.output_folder_name + file_name , index=False, header=False)
+
+                file_num+=1
+                X_i_all = pd.DataFrame()
+
+        print("data generation complete...")
+        
+      
